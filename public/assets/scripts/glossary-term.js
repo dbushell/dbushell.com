6401b141e2a3d1b5e3a75a01871c567b35953c53
David Bushell <david@dbushell.com>
2025-06-25 15:18:47 +0100

    blog (svgo)

--- public/assets/scripts/glossary-term.js
+++ public/assets/scripts/glossary-term.js
@@ -223,7 +223,7 @@ export class Component extends HTMLElement {
 <div role="tooltip" popover>
 <button class="Button" type="button" popovertargetaction="hide">
   <span class="Hidden">hide tooltip</span>
-  <svg role="presentation" viewBox="0 0 16 16">
+  <svg aria-hidden="true" viewBox="0 0 16 16">
     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
   </svg>
 </button>
